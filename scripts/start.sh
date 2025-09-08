7c4b65f7366bf1fdbf13e2efee6f0e3919761674
Muzamil <strikerrdx2000@gmail.com>
2025-09-08 14:40:59 +0500

    addec ecr fetching dynamically

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPOSITORY_URI=$(cat repository.txt)
-CONTAINER_NAME=$(basename $REPOSITORY_URI)
+# Get repository URI dynamically
+REPOSITORY_URI=$(aws ecr describe-repositories \
+  --repository-names devsecops-intern-assignment \
+  --query 'repositories[0].repositoryUri' \
+  --output text)
+
+CONTAINER_NAME=$(basename "$REPOSITORY_URI")
 IMAGE="$REPOSITORY_URI:latest"
-PORT=80  # Map container port 3000 to host port 80
+PORT=80
 
 echo "Running new container $CONTAINER_NAME from image $IMAGE..."
 docker run -d --name $CONTAINER_NAME -p $PORT:3000 $IMAGE
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Derive container name from REPOSITORY_URI
-# Example: 637423405167.dkr.ecr.us-east-1.amazonaws.com/surge-repo -> surge-repo
-REPOSITORY_URI=$(cat repository.txt)
-CONTAINER_NAME=$(basename $REPOSITORY_URI)
+# Get repository URI dynamically
+REPOSITORY_URI=$(aws ecr describe-repositories \
+  --repository-names devsecops-intern-assignment \
+  --query 'repositories[0].repositoryUri' \
+  --output text)
 
-if docker ps -q --filter "name=$CONTAINER_NAME" | grep -q .; then
-    echo "Stopping existing container $CONTAINER_NAME..."
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
-else
-    echo "No existing container to stop."
-fi
+CONTAINER_NAME=$(basename "$REPOSITORY_URI")
+
+echo "Stopping and removing old container: $CONTAINER_NAME..."
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
