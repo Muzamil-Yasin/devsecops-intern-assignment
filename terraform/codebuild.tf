52915e1ae17df28cd19c68d103609a4a26b76a05
Muzamil <strikerrdx2000@gmail.com>
2025-11-04 14:00:03 +0500

    fix codebuild

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -3,21 +3,20 @@ resource "aws_codebuild_project" "cicd_build_project" {
   description  = "CodeBuild project for building and pushing Docker images from CodePipeline"
   service_role = aws_iam_role.codebuild_service_role.arn
 
-  #  Artifacts and source both must be CODEPIPELINE
   artifacts {
     type = "CODEPIPELINE"
   }
 
   source {
-    type = "CODEPIPELINE"
-    buildspec = "buildspec.yml"  # still points to your repo file
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
   }
 
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
     image           = "aws/codebuild/standard:7.0"
     type            = "LINUX_CONTAINER"
-    privileged_mode = true # needed for Docker-in-Docker
+    privileged_mode = true
 
     environment_variable {
       name  = "AWS_REGION"
@@ -26,14 +25,16 @@ resource "aws_codebuild_project" "cicd_build_project" {
 
     environment_variable {
       name  = "ECR_REPO_URI"
-      value = "052869605945.dkr.ecr.us-east-1.amazonaws.com/devsecops-intern-assignment"
+      value = aws_ecr_repository.app_repo.repository_url
     }
 
     environment_variable {
       name  = "APP_NAME"
-      value = "my-node-app"
+      value = "devsecops-intern-assignment"
     }
   }
+
+  timeout_in_minutes = 30
 
   tags = {
     Name        = "CI-CD-Application-Build"
